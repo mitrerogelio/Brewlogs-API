f3acb3419ed2cddc7c2e737f5ea98f2e03bf4d11
Rogelio <rogelio.mitre@cambriausa.com>
2023-12-11 10:26:52 -0600

    remove migrations, add connectionstring, update brewlog model

--- brewlogsMinimalApi/Model/Brewlog.cs
+++ brewlogsMinimalApi/Model/Brewlog.cs
@@ -1,24 +1,15 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace brewlogsMinimalApi.Model;
 
 public class Brewlog
 {
-    [Key] public Guid Id { get; init; }
+    [Key] public int Id { get; set; }
+    public int Author { get; set; }
     public string CoffeeName { get; set; } = "";
     public int Dose { get; set; }
     public string Grind { get; set; } = "";
     public int BrewRatio { get; set; }
     public string Roast { get; set; } = "";
     public string BrewerUsed { get; set; } = "";
-
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    [JsonIgnore]
-    public DateTime? CreatedAt { get; init; }
-
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    [JsonIgnore]
-    public DateTime? LastUpdated { get; init; }
 }
